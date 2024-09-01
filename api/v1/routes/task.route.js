403c63dd3753747bcff196d986ee6f916092162e
thuong204 <thuongtc.22it@vku.udn.vn>
2024-09-01 15:16:53 +0700

    Them sua xoa va xoa nhieu cong viec

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -7,4 +7,8 @@ const router = express.Router()
 router.get("/", controller.index)
 router.get("/detail/:id", controller.detail)
 router.patch("/change-status/:id", controller.changeStatus)
+router.patch("/change-multi", controller.changeMulti)
+router.post("/create",controller.create)
+router.patch("/update/:id", controller.update)
+router.delete("/delete/:id", controller.delete)
 module.exports =router
--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -15,7 +15,7 @@ module.exports.index = async (req, res) => {
     //Pagination
     const initPagination = {
         currentPage: 1,
-        limitItems: 2
+        limitItems: 10
     }
     const countTasks = await Task.countDocuments(find)
     const objectPagination = pagination(initPagination, req.query, countTasks)
@@ -66,6 +66,96 @@ module.exports.changeStatus = async (req, res) => {
         res.json({
             code: 400,
             message: "Không tồn tại"
+        })
+
+    }
+
+}
+module.exports.changeMulti = async (req, res) => {
+    try {
+        const { ids, key, value } = req.body
+        switch (key) {
+            case "status":
+                await Task.updateMany({
+                    _id: { $in: ids }
+                }, {
+                    status: value
+                })
+                res.json({
+                    code: 200,
+                    message: "Cập nhật trạng thái thanhff công"
+                })
+                break;
+            case "delete":
+                await Task.deleteMany({
+                    _id: { $in: ids }
+                })
+                res.json({
+                    code: 200,
+                    message: "Xóa nhiều công viêc thành công"
+                })
+                break;
+
+            default:
+
+                res.json({
+                    code: 400,
+                    message: "Không tồn tại"
+                })
+                break;
+        }
+    } catch (error) {
+
+    }
+}
+module.exports.create = async (req, res) => {
+    try {
+        const task = new Task(req.body)
+        const data = await task.save()
+        res.json({
+            code: 200,
+            message: "Thêm công việc thành công",
+            data: data
+        })
+
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi"
+        })
+
+    }
+
+}
+module.exports.update = async (req, res) => {
+    try {
+        await Task.updateOne({
+            _id: req.params.id
+        },req.body)
+        res.json({
+            code: 200,
+            message: "Cập nhật công việc thành công"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi"
+        })
+
+    }
+
+}
+module.exports.delete= async (req, res) => {
+    try {
+        await Task.deleteOne({_id: req.params.id})
+        res.json({
+            code: 200,
+            message: "Xóa công việc thành công"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi"
         })
 
     }
